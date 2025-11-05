a86034339620a83b1e692dfb3c62a3b1fe8e6de3
a1hena <102157679+a1hena@users.noreply.github.com>
2025-11-05 23:57:08 +0900

    Update main.go

--- main.go
+++ main.go
@@ -64,6 +64,7 @@ type PluginManifest struct {
 	RepoURL                string   `json:"RepoUrl,omitempty"`
 	ApplicableVersion      string   `json:"ApplicableVersion,omitempty"`
 	DalamudApiLevel        int      `json:"DalamudApiLevel"`
+	TestingDalamudApiLevel int      `json:"TestingDalamudApiLevel"`
 	DownloadCount          int64    `json:"DownloadCount,omitempty"`
 	LastUpdate             int64    `json:"LastUpdate,omitempty"`
 	DownloadLinkInstall    string   `json:"DownloadLinkInstall,omitempty"`
